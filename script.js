104f915e4b2438d05efa0348fa4db5cd695d033a
dhamoleom9-spec <dhamoleom9@gmail.com>
2025-10-27 22:55:13 +0530

    java-start3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hay tjis is my first js file in cohort 2.0");
+console.log("hay this is my first js file in cohort 2.0");
 prompt("you want to enter..!");
 alert("hay you entered in js world");
 
@@ -26,3 +26,74 @@ a = obj;
 // null + 1 = 0+1 = 1
 // 5 + "5" = concatenation = 55
 // !!undefined = original value = false
+
+// question 1
+let fruit = "mango";
+console.log("my favorite fruit is : " + fruit);
+
+// question 2
+const dobyear = 2005;
+const cyear = 2025;
+const age = cyear-dobyear;
+console.log(age); 
+
+// question 3
+let string = "l love javascript progtamming";
+let slicestring = string.slice(7,18);
+console.log(slicestring); 
+
+// question 4
+let football = "Football is my favorite sport";
+console.log(football);
+let cricket = football.replace("Football","Cricket");
+console.log(cricket);
+
+// question 5
+let world = "hello world, world is beautiful"
+console.log(world)
+let earth = world.replaceAll("world", "earth")
+console.log(earth)
+
+// question 6
+let str = "apple,banana,grapes,mango";
+console.log(str)
+let arr = str.slice()
+console.log(arr)
+
+// question 7
+let x = 2;
+let y = 2;
+console.log(x=x,y=y)
+console.log(x+y)
+console.log(x-y)
+console.log(x/y)
+console.log(x*y)
+
+
+// question 8
+let one = 99;
+console.log(typeof(one))
+
+let two = "ram ram"
+console.log(typeof(two))
+
+let three = true
+console.log(typeof(three))
+
+let four = null
+console.log(typeof(four))
+
+let five = undefined
+console.log(typeof(five))
+
+// question 10
+let firstname = "aditya";
+let lastname = "dhotre";
+let fullname = firstname + " " + lastname;
+console.log(fullname);
+
+// question 9
+let java = "JavaScript is awesome"
+console.log(java)
+let aws = java.slice(14,21)
+console.log(aws)
