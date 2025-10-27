d85cff3cf6cecd4f90af4f6fe8c9006bf5226801
dhamoleom9-spec <dhamoleom9@gmail.com>
2025-10-27 10:42:40 +0530

    java-start

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("hay tjis is my first js file in cohort 2.0");
+prompt("you want to enter..!");
+alert("hay you entered in js world");
+
+let a = 12;  //--primative data types those which has no brachets, and another var can have copy of it
+b = a;
+
+let obj = { // thos have brachets that ae nonprimative or refrances, they gate refrance of their parent when we change an a then obj also changed and viceversa
+    name : "om sunil dhamole"
+}
+a = obj;
+
